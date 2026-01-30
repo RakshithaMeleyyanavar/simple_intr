cb27624a027c6ad698e4d750e6ae193ba2ab5e9e
Rakshitha Meleyyanavar <raksh9175@gmail.com>
2026-01-30 10:01:41 +0530

    update
    Fix syntax errors and improve variable declarations.

---
+++ simple_intr.java
@@ -0,0 +1,16 @@
+import java.util.scannner
+public class simple_intr
+{
+      int principle =0;
+      int rate =0;
+      int time =0;
+      Scannersc = new Scanner(System.in);
+      System.out.println("Enter the priciple amount");
+      principle = sc.nextInt()
+     System.out.println("Enter the rate");
+     rate = sc.nextInt()
+     System.out.println("Enter the time");
+     time = sc.nextInt()
+     Intrest = (principle* rate*time) ;
+     System.out.println("the simple intrest is an :"+Intrest)
+}
